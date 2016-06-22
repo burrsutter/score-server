9c98e831ee3d05261954aaa8dadd17c5cbde6534
Kevin Conner <kconner@redhat.com>
2016-06-22 08:24:42 -0700

    Update probe to wait for the container

--- score/configuration/probe.sh
+++ score/configuration/probe.sh
@@ -2,7 +2,8 @@
 
 for ((i=1; i<=30; i++))
 do
-    if [ "$(curl -s -u "${KIE_SERVER_USER}:${KIE_SERVER_PASSWORD}" http://$(hostname):8080/kie-server/services/rest/server -w %{http_code} -o /dev/null)" = "200" ] ; then
+    curl -s -u "${KIE_SERVER_USER}:${KIE_SERVER_PASSWORD}" http://$(hostname):8080/kie-server/services/rest/server/containers | grep -q SUCCESS
+    if [ $? -eq 0 ] ; then
         echo finished
         exit 0
     fi
